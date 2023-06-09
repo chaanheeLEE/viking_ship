7fa9deb1e650b640f6ffe013bb6680e4e26744ff
chaanheeLEE <74493097+chaanheeLEE@users.noreply.github.com>
2023-06-09 19:43:30 +0900

    modify treeCollision.cs

--- viking_ship/Assets/Scripts/TreeCollision.cs
+++ viking_ship/Assets/Scripts/TreeCollision.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TreeCollision : MonoBehaviour
 {
     private WeaponPower weaponPower;
-    public GameObject tree;
+    public GameObject tree = null;
     public float treeHP;
     private Vector3 angle;
     public PLAYER_INFO user;
@@ -16,11 +16,6 @@ public class TreeCollision : MonoBehaviour
         weaponPower = FindAnyObjectByType<WeaponPower>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     private void OnCollisionEnter(Collision collision)//충돌발생시
     {
         //태그가 weapon임을 확인
@@ -35,10 +30,10 @@ public class TreeCollision : MonoBehaviour
                     = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotationY; //리지드바디 제약(포지션, 로테이션)
                 
                 ContactPoint contactPoint = collision.GetContact(0); //충돌 지점 구하고
-                angle = -contactPoint.normal;    //노말벡터
+                angle = contactPoint.normal;    //노말벡터
                 tree.transform.rotation = Quaternion.Euler(angle*25); //공격받은 각도로 넘어지도록 각도 조정
                 
-                Destroy(tree, 15.0f);
+                Destroy(tree, 10.0f);
                 user.items[0].value +=10;
             }
         }

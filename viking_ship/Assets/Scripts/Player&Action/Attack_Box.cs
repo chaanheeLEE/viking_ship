b77fc0642b5e85ab6b6e45eb8714c73270dba446
JBL28 <120627991+JBL28@users.noreply.github.com>
2023-06-11 16:14:05 +0900

    JB_000
    무기 스킬기능 추가입니다.

---
+++ viking_ship/Assets/Scripts/Player&Action/Attack_Box.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Box : MonoBehaviour
+{
+    private int damage;
+
+    private void OnCollisionStay(Collision collision)
+    {
+        damage = transform.parent.GetComponent<Attack>().Damage;
+
+        if(collision.gameObject.tag == "enemy"){
+            collision.gameObject.SendMessage("Attacked", damage);
+        }
+
+    }
+}
